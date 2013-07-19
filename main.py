fbdf82336b9f5e4cd2df768c20cf5188523703f2
Esa Varemo <esa@kuivanto.fi>
2013-07-19 20:34:29 +0300

    Fixed missing pass

--- main.py
+++ main.py
@@ -78,39 +78,39 @@ def getData():
     except socket.error, e:
         err = e.args[0]
         if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+    	    pass # no data
     
 def main():
+    settings = parse_config()
 
-	settings = parse_config()
+    try:
+	connect(settings)
+    except Exception as e:
+	print e
 
-	try:
-		connect(settings)
-	except Exception as e:
-		print e
+    sleep(1)
 
-	sleep(1)
+    while True:
+	ircmsg = getData()
 
-	while True:
-		ircmsg = getData()
+	if not len(ircmsg) == 0:
+	    print ircmsg
 
-		if not len(ircmsg) == 0:
-		    print ircmsg
+	    if ircmsg.find('PING ') != -1:
+		ircsock.send('PING :Pong\n')
 
-		    if ircmsg.find('PING ') != -1:
-			ircsock.send('PING :Pong\n')
-
-		    result = re.search(':(.*)!.* JOIN #herramustikka', ircmsg)
-		    if not result == None:
-			nick = result.group(1)
-			print("Found a viewer joining: " + nick + "\n")
-			#print("\n\n" + result.group(1) + "\n\n")
-			msg = 'PRIVMSG #herramustikka :Tervetuloa ' + nick + "\n"
-			print("SENDING: " + msg)
-			#ircsock.send(msg)
+	    result = re.search(':(.*)!.* JOIN #herramustikka', ircmsg)
+	    if not result == None:
+		nick = result.group(1)
+		print("Found a viewer joining: " + nick + "\n")
+		#print("\n\n" + result.group(1) + "\n\n")
+		msg = 'PRIVMSG #herramustikka :Tervetuloa ' + nick + "\n"
+		print("SENDING: " + msg)
+		#ircsock.send(msg)
 		
-		    if ircmsg.find(' PRIVMSG ') != -1:
-			nick = ircmsg.split('!')[0][1:]
-			msg = ircmsg.split(' PRIVMSG ')[-1].split(' :')[1]
+	    if ircmsg.find(' PRIVMSG ') != -1:
+		nick = ircmsg.split('!')[0][1:]
+		msg = ircmsg.split(' PRIVMSG ')[-1].split(' :')[1]
 
 main()
 
