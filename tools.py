a5a303edcfaea1ded22b3ea13097d6f72ba13a2b
Esa Varemo <esa@kuivanto.fi>
2013-08-08 19:57:25 +0300

    Docstring'ed tools.py Working on ISSUE #50: Add docstrings to undocumented methods

--- tools.py
+++ tools.py
@@ -2,5 +2,13 @@ import re
 
 
 def stripPrefix(text):
+    """
+    :param text: text to remove the prefix from
+    :type text: str
+    :return: passed text without the prefix
+    :rtype: str
+
+    Strip a prefix (the bots name) from commands
+    """
     text = re.sub(r'![Mm]ustikka[Bb]ot (.*)', r'!\1', text)
     return text
