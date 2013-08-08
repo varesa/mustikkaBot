25f44254822b8a89872b87c6bc0952b243cfdfc0
Esa Varemo <esa@kuivanto.fi>
2013-08-08 21:07:21 +0300

    Docstring'ed modulemanager.py Working on ISSUE #50: Add docstrings to undocumented methods

--- modulemanager.py
+++ modulemanager.py
@@ -8,11 +8,25 @@ class modulemanager:
     bot = None
 
     def init(self, bot):
+        """
+        :param bot:  Reference to the main bot instance
+        :type bot: bot
+
+        Initialize the module manager
+        """
         self.bot = bot
         self.setupModules()
         self.initModules()
 
     def importModule(self, file):
+        """
+        :param file: path to the module to be imported
+        :type file: str
+        :return: imported module
+        :rtype: module
+
+        Load a single module from disk
+        """
         fpath = os.path.normpath(os.path.join(os.path.dirname(__file__), file))
         dir, fname = os.path.split(fpath)
         mname, ext = os.path.splitext(fname)
@@ -21,6 +35,9 @@ class modulemanager:
         return imp.load_module(mname, file, filename, data)
 
     def setupModules(self):
+        """
+        Go through the modules on disk importing them
+        """
         files = os.listdir("modules/")
         for file in files:
             result = re.search(r'\.py$', file)
@@ -30,6 +47,12 @@ class modulemanager:
                 self.modules[id] = getattr(module, id)()
 
     def addModule(self, name):
+        """
+        :param name: Name of the module
+        :type name: str
+
+        Load a module by name
+        """
         file = "modules/" + name + ".py"
         if os.path.exists(file):
             module = self.importModule(file)
@@ -37,14 +60,37 @@ class modulemanager:
             self.modules[id] = getattr(module, id)()
 
     def removeModule(self, id):
+        """
+        :param id: Id of the module
+        :type id: str
+
+        Unload a module by name
+        """
         self.modules.pop(id, None)
 
     def initModules(self):
+        """
+        Go through loaded modules initializing them
+        """
         for name, module in self.modules.items():
             module.init(self.bot)
 
     def getModule(self, name):
+        """
+        :param name: name of the module
+        :type name: str
+        :return: reference to the module
+        :rtype: module
+
+        Return a reference to specified module
+        """
         return self.modules[name]
 
     def getModules(self):
+        """
+        :return: A dict of all loaded modules
+        :rtype: dict(str:module)
+
+        Return a dictionary of all the loaded modules {"id":module,...}
+        """
         return self.modules
