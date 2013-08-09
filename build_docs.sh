26d0f3cf2d008566dbfa732f76ab053035190752
Esa Varemo <esa@kuivanto.fi>
2013-08-09 12:57:33 +0300

    Made build_docs work on both of my computers

--- build_docs.sh
+++ build_docs.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-sphinx-build docs_src/ docs_build/
+if [ `command -v sphinx-build` ]; then 
+    sphinxcmd=sphinx-build
+elif [ `command -v sphinx-build-3.3` ]; then
+    sphinxcmd=sphinx-build-3.3
+fi
+
+$sphinxcmd docs_src/ docs_build/
