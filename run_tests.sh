5403665f84b858a52d2c6e84eda564f4d1c8c7a0
Esa Varemo <esa@kuivanto.fi>
2013-08-24 19:39:43 +0300

    Find out what version of nose to run

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-nosetests -v $1
+for dir in ${PATH//:/ }; do
+  CMDS="$CMDS `ls $dir/nosetests-* 2>/dev/null`"
+done
+
+CMDS=`echo $CMDS | sed -e 's/^ *//g' -e 's/ *$//g'`
+
+if [ -z "$CMDS" ]; then
+    if [ `command -v nosetests` ]; then
+        testcmd=nosetests
+    else
+        echo "No versions of Nose found"
+    fi
+else
+    CMDS=(`echo $CMDS |xargs -n1|sort -r`)
+    testcmd=${CMDS[0]}
+fi
+
+echo "Using nose version: $testcmd"
+$testcmd -v $1
