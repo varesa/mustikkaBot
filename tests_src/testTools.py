4bcd2b6332dc8fb3cfa35813405431d9433b0c90
Esa Varemo <esa@kuivanto.fi>
2013-08-24 14:53:47 +0300

    Starting on ISSUE #49: Create unit tests test stripPrefix()

---
+++ tests_src/testTools.py
@@ -0,0 +1,20 @@
+import tools
+
+def test_stripPrefix_cases():
+    """test?"""
+    test1 = "!mustikkabot a"
+    test2 = "!MustikkaBot a"
+    test3 = "!Mustikkabot a"
+    test4 = "!mustikkaBot a"
+
+    assert tools.stripPrefix(test1) == "!a"
+    assert tools.stripPrefix(test2) == "!a"
+    assert tools.stripPrefix(test3) == "!a"
+    assert tools.stripPrefix(test4) == "!a"
+
+def test_stripPrefix_nomatch():
+    test1 = "!mustikkaot a"
+    test2 = "MustikkaBot a"
+
+    assert tools.stripPrefix(test1) == test1
+    assert tools.stripPrefix(test2) == test2
