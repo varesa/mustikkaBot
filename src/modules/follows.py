543047c248c8ad1c04954e68da5580db54ce4c80
Esa Varemo <esa@kuivanto.fi>
2014-02-27 03:52:12 +0200

    Working on ISSUE #121: Shout out follows Testing follower shouting

--- src/modules/follows.py
+++ src/modules/follows.py
@@ -1,8 +1,11 @@
 import logging
 import datetime
+from TwitchAPI.Channel import Channel
 
-def test():
-    print("abc")
+
+def _ts2dt(timestamp):
+    return datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+
 
 class Follows:
 
@@ -10,8 +13,22 @@ class Follows:
     bot = None
     """ :type: bot"""
 
+    last_created_at = 0
+
     def check_followers(self):
-        self.log.debug("Interval!")
+        data = Channel("herramustikka").get_followers()
+
+        if self.last_created_at == 0:
+            self.last_created_at = _ts2dt(data[0]['created_at'])
+            return
+
+        #print(data[0])
+
+        for follow in data[::-1]:
+
+            if _ts2dt(follow['created_at']) > self.last_created_at:
+                print("New follower: " + follow['user']['display_name'])
+                self.last_created_at = _ts2dt(follow['created_at'])
 
     def init(self, bot):
         """
